88e60ea222504cc709690695a41f55a9bc587c3a
Muhammad Ainuddin <Payohngat@gmail.com>
2025-06-18 07:25:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'image.slidesharecdn.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.mcrmedical.com', // Added this hostname
+        port: '',
+        pathname: '/**',
       }
     ],
   },
