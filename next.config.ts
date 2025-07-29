840eca9b4722a7a565c7f70e3abdbb631135ce44
Muhammad Ainuddin <Payohngat@gmail.com>
2025-07-29 08:03:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -70,6 +70,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'post.healthline.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
